40275215dc40094252107cdbb5b8990262581ead
Maximino Castillo <maxcastgarcia@gmail.com>
2025-03-18 21:35:04 -0600

    Add route system with lazyloading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'recative',
+    loadChildren: () => import('./reactive/reactive.routes'),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
+  },
+  {
+    path: 'country',
+    loadChildren: () => import('./country/country.routes'),
+  },
+  {
+    path: '**',
+    redirectTo: 'reactive'
+  }
+];
---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+
+export const countryRoutes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'sign-up',
+        component: CountryPageComponent
+      }
+    ]
+  }
+];
+
+export default countryRoutes;

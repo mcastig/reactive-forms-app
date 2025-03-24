a42df7fcd13fa7171cc5df143136e101b26a7a2d
Maximino Castillo <maxcastgarcia@gmail.com>
2025-03-23 23:19:41 -0600

    Improve form validations with formbuilder

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -8,9 +8,48 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './basic-page.component.html',
 })
 export class BasicPageComponent {
-  public myForm = new FormGroup({
-    name: new FormControl(''),
-    price: new FormControl(0),
-    inStorage: new FormControl(0),
+  private fb = inject(FormBuilder);
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(10)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]]
   });
+
+  public isValidField(fieldName: string): boolean | null {
+    return (
+      this.myForm.controls[fieldName].errors &&
+      this.myForm.controls[fieldName].touched
+    );
+  }
+
+  public getFieldError(fieldName: string): string | null {
+    if(!this.myForm.controls[fieldName]) return null;
+
+    const errors = this.myForm.controls[fieldName].errors ?? {};
+
+    for( const key of Object.keys(errors)) {
+      switch(key) {
+        case 'required':
+          return 'This field is required.';
+        case 'minlength':
+          return `This field requires at least ${ errors['minlength'].requiredLength } characters.`;
+        case 'min':
+          return `Minimum value required ${ errors['min'].min }`;
+      }
+    }
+    return null;
+  }
+
+  public onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    this.myForm.reset({
+      price: 0,
+      inStorage: 0,
+    });
+  }
 }
